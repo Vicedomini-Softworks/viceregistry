ecd79226e92fa4819306ab3eb6167d4723278dba
Sofia Vicedomini <11537307+blacksoulgem95@users.noreply.github.com>
2026-04-28 21:46:55 +0200

    fix: simplify nginx config, remove entrypoint migrations

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,6 @@
 #!/bin/sh
 set -e
 
-echo "Running migrations..."
-npm run db:migrate
-
-echo "Running seeds..."
-npm run db:seed
-
 echo "Generating nginx config (PORT=${PORT})..."
 envsubst '${PORT} ${REGISTRY_URL}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
